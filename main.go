f29ae4f819501d0995e1c676cfa5d8de9a3a4c87
Maedchen08 <ratihsarifajar18@gmail.com>
2021-10-01 20:16:54 +0700

    cek

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	app := fiber.New()
 	app.Get("/:userid/detail", GetByUserId)
 	app.Get("/follower/:username", GetByUsername)
-	app.Listen(":"+ os.Getenv("PORT"))
+	app.Listen(":" + os.Getenv("PORT"))
 	// app.Listen(":8989")
 
 }
@@ -25,11 +25,10 @@ var dataUser = []DataUser{
 	{UserId: "jesse", Username: "JesseOctopus", Followers: 432},
 	{UserId: "jamie", Username: "JamieMantisShrimp", Followers: 654},
 	{UserId: "drew", Username: "DrewSquid", Followers: 321},
-
 }
 
 func GetByUserId(c *fiber.Ctx) error {
-	userId := c.Params("userid") 
+	userId := c.Params("userid")
 	for _, data := range dataUser {
 		if data.UserId == userId {
 			return c.Status(fiber.StatusCreated).JSON((fiber.Map{
